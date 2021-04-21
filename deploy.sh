a6f11aa28f365491397d7fbaab5fbe22e5a48016
Sof <skrbicsofija@gmail.com>
2021-04-21 13:49:15 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:sofskrbic/<REPO>.git main:gh-pages
 
 cd -
